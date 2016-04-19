7323689bf322baac96398f85ddfdfb91a15e4517
Samuel Cozannet <samnco@gmail.com>
2016-04-19 18:51:02 +0200

    huge refactoring to manage multi architectures

--- reactive/dl4j.sh
+++ reactive/dl4j.sh
@@ -3,16 +3,24 @@ set -ex
 
 source charms.reactive.sh
 
-# 'apt.installed.cmake' 'apt.installed.maven3'
-@when_not 'dl4j.installed' 
-@when 'cuda.available' 
-function install_dl4j() {
-    status-set maintenance "Installing dl4j software"
+#####################################################################
+#
+# Simple functions
+#
+#####################################################################
 
-    juju-log "Installing dependencies"
-    # Note most deps are installed from the layer.yaml options
-    # Then the rest from the apt layer
+# Install is guaranteed to run once per rootfs
+function bash::lib::get_ubuntu_codename() {
+    lsb_release -a 2>/dev/null | grep Codename | awk '{ print $2 }'
+}
 
+#####################################################################
+#
+# Prepare the machine per architecture
+# 
+#####################################################################
+
+function all::all::install_prerequisites() {
     # We need maven 3.2.3+ which is not in repos. Installing if necessary
     hash mvn 2>/dev/null || apt-get remove --purge maven 
 
@@ -27,85 +35,151 @@ function install_dl4j() {
         openjdk-8-jre-headless \
         openjdk-8-jdk
 
-    case "$(arch)" in 
-        "x86_64" | "amd64" )
-            apt-get install -yqq \
-                cmake \
-                libopenblas-base \
-                libopenblas-dev
-        ;;
-        "ppc64le" )
-            cd /mnt
-            wget -c https://cmake.org/files/v3.5/cmake-3.5.2.tar.gz 
-            tar xfz cmake-3.5.2.tar.gz
-            cd cmake-3.5.2
-            ./bootstrap
-            make && make install 
-
-        [ -d "/mnt/openblas" ] \
-            || git clone https://github.com/xianyi/OpenBLAS.git /mnt/openblas \
-            && { cd "/mnt/openblas" ; git pull origin master ; cd - ; }
-            cd /mnt/openblas
-            make && make PREFIX=/usr install
-        ;;
-        "*" )
-            juju-log "Your architecture is not supported yet. Exiting"
-            exit 1
-        ;;
-    esac
-
-
-    juju-log "Creating program variables"
-    export LIBND4J_HOME="/mnt/libnd4j"
-    echo 'export LIBND4J_HOME="/mnt/libnd4j"' | tee /etc/profile.d/libnd4j.sh
-    chmod +x /etc/profile.d/libnd4j.sh
-
     juju-log "Downloading required projects"
-    for PROJECT in nd4j libnd4j deeplearning4j Canova
+    for PROJECT in libnd4j nd4j deeplearning4j Canova
     do
         [ -d "/mnt/${PROJECT}" ] \
             || git clone -q https://github.com/deeplearning4j/${PROJECT}.git "/mnt/${PROJECT}" \
-            && { cd "/mnt/${PROJECT}" ; git pull origin master ; cd - ; }
+            && { cd "/mnt/${PROJECT}" ; git pull ; }
     done 
     
     [ -d "/mnt/javacpp" ] \
         || git clone -q https://github.com/bytedeco/javacpp.git /mnt/javacpp \
-        && { cd "/mnt/javacpp" ; git pull origin master ; cd - ; }
-
-    juju-log "Installing Java CPP"
+        && { cd "/mnt/javacpp" ; git pull ; }
     cd /mnt/javacpp
     mvn clean install -DskipTests
+}
 
-    juju-log "Installing libnd4j for CPU & GPU"
+function trusty::x86_64::install_prerequisites() {
+    apt-get install -yqq \
+        cmake \
+        libopenblas-base \
+        libopenblas-dev
+}
+
+function xenial::x86_64::install_prerequisites() {
+    apt-get install -yqq \
+        cmake \
+        libopenblas-base \
+        libopenblas-dev
+}
+
+function trusty::ppc64le::install_prerequisites() {
+    cd /mnt
+    wget -c https://cmake.org/files/v3.5/cmake-3.5.2.tar.gz 
+    tar xfz cmake-3.5.2.tar.gz
+    cd cmake-3.5.2
+    ./bootstrap
+    make && make install 
+
+    [ -d "/mnt/openblas" ] \
+    || git clone https://github.com/xianyi/OpenBLAS.git /mnt/openblas \
+    && { cd "/mnt/openblas" ; git pull ; cd - ; }
+    cd /mnt/openblas
+    make && make PREFIX=/usr install
+}
+
+function xenial::ppc64le::install_prerequisites() {
+    cd /mnt
+    wget -c https://cmake.org/files/v3.5/cmake-3.5.2.tar.gz 
+    tar xfz cmake-3.5.2.tar.gz
+    cd cmake-3.5.2
+    ./bootstrap
+    make && make install 
+
+    [ -d "/mnt/openblas" ] \
+    || git clone https://github.com/xianyi/OpenBLAS.git /mnt/openblas \
+    && { cd "/mnt/openblas" ; git pull ; cd - ; }
+    cd /mnt/openblas
+    make && make PREFIX=/usr install
+}
+
+#####################################################################
+#
+# Install libnd4j per architecture
+# 
+#####################################################################
+
+function all::all::install_libnd4j() {
+    export LIBND4J_HOME="/mnt/libnd4j"
+    echo 'export LIBND4J_HOME="/mnt/libnd4j"' | sudo tee /etc/profile.d/libnd4j.sh
+
     cd ${LIBND4J_HOME}
     for TARGET in cpu cuda
     do
         ./buildnativeoperations.sh blas ${TARGET}
     done 
+}
+
+#####################################################################
+#
+# Install ND4j, DL4j, and Canova per architecture
+# 
+#####################################################################
+
+function trusty::x86_64::install_dl4j() {
+    for PROJECT in nd4j deeplearning4j Canova
+    do
+        cd "/mnt/${PROJECT}"
+        JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64" mvn clean install -DskipTests -Dmaven.javadoc.skip=true
+    done
+}
+
+function xenial::x86_64::install_dl4j() {
+    for PROJECT in nd4j deeplearning4j Canova
+    do
+        cd "/mnt/${PROJECT}"
+        JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64" mvn clean install -DskipTests -Dmaven.javadoc.skip=true
+    done
+}
+
+function trusty::ppc64le::install_dl4j() {
+    for PROJECT in nd4j deeplearning4j Canova
+    do
+        cd "/mnt/${PROJECT}"
+        JAVA_HOME="/usr/lib/jvm/java-8-openjdk-ppc64lel" mvn clean install -DskipTests -Dmaven.javadoc.skip=true
+    done
+}
+
+function xenial::ppc64le::install_dl4j() {
+    for PROJECT in nd4j deeplearning4j Canova
+    do
+        cd "/mnt/${PROJECT}"
+        JAVA_HOME="/usr/lib/jvm/java-8-openjdk-ppc64lel" mvn clean install -DskipTests -Dmaven.javadoc.skip=true
+    done
+}
+
+UBUNTU_CODENAME="$(bash::lib::get_ubuntu_codename)"
+
+
+# 'apt.installed.cmake' 'apt.installed.maven3'
+@when_not 'dl4j.installed' 
+@when 'cuda.available' 
+function install_dl4j() {
+    status-set maintenance "Installing dl4j software"
+
+    juju-log "Installing dependencies"
+    all::all::install_prerequisites
 
     case "$(arch)" in 
         "x86_64" | "amd64" )
-            juju-log "Installing DL4j"
-            for PROJECT in nd4j deeplearning4j Canova
-            do
-                cd "/mnt/${PROJECT}"
-                JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64" mvn clean install -DskipTests -Dmaven.javadoc.skip=true
-            done
+            ARCH="x86_64"
         ;;
         "ppc64le" )
-            juju-log "Installing DL4j"
-            for PROJECT in nd4j deeplearning4j Canova
-            do
-                cd "/mnt/${PROJECT}"
-                JAVA_HOME="/usr/lib/jvm/java-8-openjdk-ppc64el" mvn clean install -DskipTests -Dmaven.javadoc.skip=true
-            done
+            ARCH="$(arch)"
         ;;
         "*" )
             juju-log "Your architecture is not supported yet. Exiting"
             exit 1
         ;;
     esac
+    ${UBUNTU_CODENAME}::${ARCH}::install_prerequisites
+    
+    juju-log "Installing DL4j & Other libs"
+    all::all::install_libnd4j
+    ${UBUNTU_CODENAME}::${ARCH}::install_dl4j
 
+    juju-log "Moving Maven Repo to /mnt/.m2 and making readable"
     cp -r "${HOME}/.m2" /mnt/
     chmod -R a+r /mnt/.m2
 
